2685c30b941a82eb891038ba41bdbbb30dca675c
Julian Engel <julianengel@users.noreply.github.com>
2024-10-11 15:44:15 +0200

    Update magvar.js
    address change

--- js/magvar.js
+++ js/magvar.js
@@ -38,9 +38,8 @@ function init() {
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 15,
-
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(34.693825, 33.064656), // Our Location
+        center: new google.maps.LatLng(34.6847123, 33.0516048), // Our Location
 
         // Disables the default Google Maps UI components
         disableDefaultUI: false,
